5396718872f5a7539707712377f7b098397c6fec
Alex <alexander.p.niehaus@gmail.com>
2019-03-22 15:17:24 +0100

    finished java

--- collatz.java
+++ collatz.java
@@ -1,7 +1,23 @@
-
 public class collatz {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        
+        double num = Math.pow(10, 2);
+        int run = (int)num;
+
+        int y = 0;
+    
+        for (int i=2; i <= run; i++){
+            int x = i;
+            while (x != 1){
+                if (x % 2 == 0)
+                    x = x/2;
+                else
+                    x = 3 * x + 1;
+            }
+            y = y + 1;
+            }
+        System.out.println(y);
     }
+    
 }
